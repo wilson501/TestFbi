9b266d7f18064bacd5dc2297995eca22ad529d05
Wilson <wilson.chong501@gmail.com>
2016-01-02 03:19:11 +0800

    qwerty

--- app/src/main/java/com/wilson/hp/myapplication/MainActivity.java
+++ app/src/main/java/com/wilson/hp/myapplication/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // fbi 
+        // fbi
+        //qwerty
     }
 
 
